2c1d395074c7ac5368389785688e940f4e4acd22
Sungchool Park <grimhan2@gmail.com>
2023-03-07 20:13:33 +0900

    최신수정

--- WhatInSql/frmMain.Designer.cs
+++ WhatInSql/frmMain.Designer.cs
@@ -29,78 +29,20 @@ namespace WhatInSql
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtFilePath = new System.Windows.Forms.TextBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabFromSrc = new System.Windows.Forms.TabPage();
-            this.txtSrc = new System.Windows.Forms.RichTextBox();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.txtResult = new System.Windows.Forms.TextBox();
             this.btnSearchStart = new System.Windows.Forms.Button();
             this.txtTotalCount = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtResult2 = new System.Windows.Forms.TextBox();
-            this.tabControl1.SuspendLayout();
-            this.tabFromSrc.SuspendLayout();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.tabFromSrc = new System.Windows.Forms.TabPage();
+            this.txtSrc = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage2.SuspendLayout();
+            this.tabFromSrc.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // txtFilePath
-            // 
-            this.txtFilePath.Enabled = false;
-            this.txtFilePath.Location = new System.Drawing.Point(24, 30);
-            this.txtFilePath.Name = "txtFilePath";
-            this.txtFilePath.Size = new System.Drawing.Size(532, 21);
-            this.txtFilePath.TabIndex = 1;
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabFromSrc);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(1, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(685, 352);
-            this.tabControl1.TabIndex = 4;
-            // 
-            // tabFromSrc
-            // 
-            this.tabFromSrc.Controls.Add(this.txtSrc);
-            this.tabFromSrc.Location = new System.Drawing.Point(4, 22);
-            this.tabFromSrc.Name = "tabFromSrc";
-            this.tabFromSrc.Padding = new System.Windows.Forms.Padding(3);
-            this.tabFromSrc.Size = new System.Drawing.Size(677, 326);
-            this.tabFromSrc.TabIndex = 0;
-            this.tabFromSrc.Text = "From text";
-            this.tabFromSrc.UseVisualStyleBackColor = true;
-            // 
-            // txtSrc
-            // 
-            this.txtSrc.Location = new System.Drawing.Point(7, 6);
-            this.txtSrc.Name = "txtSrc";
-            this.txtSrc.Size = new System.Drawing.Size(664, 314);
-            this.txtSrc.TabIndex = 1;
-            this.txtSrc.Text = "";
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.txtFilePath);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(677, 326);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "From File";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // txtResult
-            // 
-            this.txtResult.Font = new System.Drawing.Font("굴림체", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtResult.Location = new System.Drawing.Point(5, 423);
-            this.txtResult.Multiline = true;
-            this.txtResult.Name = "txtResult";
-            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResult.Size = new System.Drawing.Size(681, 176);
-            this.txtResult.TabIndex = 11;
             // 
             // btnSearchStart
             // 
@@ -138,6 +80,64 @@ namespace WhatInSql
             this.txtResult2.Size = new System.Drawing.Size(681, 176);
             this.txtResult2.TabIndex = 15;
             // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("굴림체", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtResult.Location = new System.Drawing.Point(5, 423);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(681, 176);
+            this.txtResult.TabIndex = 11;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtFilePath);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(677, 326);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "From File";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Enabled = false;
+            this.txtFilePath.Location = new System.Drawing.Point(24, 30);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(532, 21);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // tabFromSrc
+            // 
+            this.tabFromSrc.Controls.Add(this.txtSrc);
+            this.tabFromSrc.Location = new System.Drawing.Point(4, 22);
+            this.tabFromSrc.Name = "tabFromSrc";
+            this.tabFromSrc.Padding = new System.Windows.Forms.Padding(3);
+            this.tabFromSrc.Size = new System.Drawing.Size(677, 326);
+            this.tabFromSrc.TabIndex = 0;
+            this.tabFromSrc.Text = "From text";
+            this.tabFromSrc.UseVisualStyleBackColor = true;
+            // 
+            // txtSrc
+            // 
+            this.txtSrc.Location = new System.Drawing.Point(7, 6);
+            this.txtSrc.Name = "txtSrc";
+            this.txtSrc.Size = new System.Drawing.Size(664, 314);
+            this.txtSrc.TabIndex = 1;
+            this.txtSrc.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabFromSrc);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(685, 352);
+            this.tabControl1.TabIndex = 4;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -151,26 +151,26 @@ namespace WhatInSql
             this.Controls.Add(this.tabControl1);
             this.Name = "frmMain";
             this.Text = "WhatInSql Desktop";
-            this.tabControl1.ResumeLayout(false);
-            this.tabFromSrc.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabFromSrc.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox txtFilePath;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabFromSrc;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.Button btnSearchStart;
         private System.Windows.Forms.TextBox txtTotalCount;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox txtSrc;
         private System.Windows.Forms.TextBox txtResult2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TabPage tabFromSrc;
+        private System.Windows.Forms.RichTextBox txtSrc;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
